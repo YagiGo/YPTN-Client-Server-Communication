1f7a1a1b4f45b8441ce589331a0636398c21518c
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-07-30 18:37:26 +0900

    Implemented websocket with Flask to transfer data between chrome extension and server

--- YPTN-Client/fetchInfo.js
+++ YPTN-Client/fetchInfo.js
@@ -5,9 +5,18 @@ function callback(requestDetails) {
 	console.log("Console Log: ",requestDetails);
 }
 
+function sendData(domain, port) {
+	let socket = io.connect('http://' + domain + ":" + port);
+	socket.on('connect', function (requuestDetails) {
+		socket.emit('my event', requuestDetails);
+    })
+}
+
+/*
 function distinguishHeaderByID(requestDetails) {
     for(let i = 0; i <requestDetails.length; i++)
 }
+*/
 // add the listener,
 // passing the filter argument and "blocking"
 // browser.webRequest.onBeforeRequest.addListener(
