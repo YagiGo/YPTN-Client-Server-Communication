19e16c587ada0f2a9ee70ea14e4801e17d3328b6
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-07-30 19:43:24 +0900

    Implemented websocket with Flask to transfer data between chrome extension and server

--- ServerSide/server.py
+++ ServerSide/server.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+# create server side event handler here
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ', message)
+# This type is preferred since it doesn't limit type
+@socketio.on('my_event')
+def handle_my_custom_event(json):
+    print("received json: ", str(json))
 
 
 
