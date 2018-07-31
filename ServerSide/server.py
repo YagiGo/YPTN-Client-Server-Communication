f50c99c9817439c7c6ce893984880c1318535297
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-07-31 16:53:46 +0900

    Page Change Detection

--- ServerSide/server.py
+++ ServerSide/server.py
@@ -6,35 +6,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+
 # create server side event handler here
 @socketio.on('message')
 def handle_message(message):
     print('received message: ', message)
+
+
 # This type is preferred since it doesn't limit type
 @socketio.on('my_event')
 def handle_my_custom_event(json):
     print("received json: ", str(json))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
--- YPTN-Client/fetchInfo.js
+++ YPTN-Client/fetchInfo.js
@@ -1,8 +1,54 @@
 /**
  * Created by zhaoxin on 18/07/26.
  */
+let tabCounter = 0; // Tab counter
+let previousRequestID = ''; //This is used to record previous request ID for same request
+
 function callback(requestDetails) {
+	// For test purpose
 	console.log("Console Log: ",requestDetails);
+}
+
+function changeMainFrame(requestDetails) {
+	// The User have left the current site
+	console.log("The User have changed the domain: ", requestDetails);
+}
+
+function changeSubFrame(requestDetails) {
+	// The user is still under the same page but in a different page
+	console.log("The user went to a different page under the same domain: ", requestDetails);
+}
+
+function openNeWTab(requestDetails) {
+	// Do something when user open a new tab
+	if(requestDetails.url === "https://www.google.co.jp/_/chrome/newtab?ie=UTF-8") {
+		tabCounter += 1;
+		console.log("The user has opened a new tab\n current tab count:", tabCounter);
+	}
+}
+
+function pageChange(requestDetails) {
+
+	if(requestDetails.url === "https://www.google.co.jp/_/chrome/newtab?ie=UTF-8") {
+		tabCounter += 1;
+		console.log("The user has opened a new tab\n current tab count:", tabCounter);
+	}
+
+	else if(previousRequestID !== requestDetails.requestId || previousRequestID === '' ) {
+		// A new event was recorded
+		previousRequestID = requestDetails.requestId;
+		console.log("New event: ", requestDetails);
+	}
+
+	else if(previousRequestID === requestDetails.requestId) {
+		// One event has multiple request
+		console.log("Same Event: ", requestDetails);
+	}
+
+	else {
+		previousRequestID = requestDetails.requestId;
+		console.log("Other Situation: ", requestDetails);
+	}
 }
 
 function sendData(domain, port) {
@@ -21,11 +67,24 @@ function distinguishHeaderByID(requestDetails) {
 // passing the filter argument and "blocking"
 // browser.webRequest.onBeforeRequest.addListener(
 chrome.webRequest.onBeforeRequest.addListener(
-	callback,
+	pageChange,
 	{urls: ["<all_urls>"], types: ["main_frame"]},
 	["blocking"]
 );
 
+chrome.webRequest.onBeforeRequest.addListener(
+	callback,
+	{urls: ["<all_urls>"]},
+	["blocking"]
+
+);
+/*
+chrome.webRequest.onBeforeRequest.addListener(
+	openNeWTab,
+	{urls: ["<all_urls>"], types: ['main_frame']},
+	['blocking']
+);
+*/
 
 /*
 chrome.webRequest.onBeforeSendHeaders.addListener(
